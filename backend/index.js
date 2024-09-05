17d8466c1a2686878319d1ca8ef0cf9536e1ef48
AbhayKumarPanchal <146933616+AbhayKumarPanchal@users.noreply.github.com>
2024-09-05 11:43:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ dotenv.config();
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(express.json({ limit: '10mb' }));
+app.use(cors());
 
 mongoose
     .connect(process.env.MONGO_URL, {
